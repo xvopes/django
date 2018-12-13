4320b646a1b432f738d7cc764739721788946fcf
xvopes <xvopes@gmail.com>
2018-12-13 17:30:16 +0800

    2

--- mysite/learn/models.py
+++ mysite/learn/models.py
@@ -1,9 +1,24 @@
+#coding:utf-8
+from django.contrib import admin
 from django.db import models
 
-class Person(models.Model):
-    name = models.CharField(max_length=30)
-    age = models.IntegerField()
 
+class Article(models.Model):
+    title = models.CharField(u'标题', max_length=256)
+    content = models.TextField(u'内容')
+
+    pub_date = models.DateTimeField(u'发表时间', auto_now_add=True, editable=True)
+    update_time = models.DateTimeField(u'更新时间', auto_now=True, null=True)
+ 
     def __str__(self):
-        return self.name
+        return self.title
 
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    def my_property(self):
+        return self.first_name + ' ' + self.last_name
+    my_property.short_description = "Full name of the person"
+    full_name = property(my_property)
+
+            
